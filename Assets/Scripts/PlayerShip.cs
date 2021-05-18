5c372631a9172575cca7459b62cb2c62f9c86db8
U-ryuryu <kugyuryuryu@gmail.com>
2021-05-18 11:05:47 +0900

    弾の発射の実装

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+    public Transform firePoint;
+    public GameObject bulletPrefab;
     // Update is called once per frame
     void Update()
+    {
+        Shot();
+        Move();
+    }
+    void Shot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+        }
+    }
+    void Move()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
