61a5b8929001df09f52033d412b649ac816cd233
U-ryuryu <kugyuryuryu@gmail.com>
2021-05-18 13:20:51 +0900

    敵の無限生成

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -7,7 +7,7 @@ public class EnemyGenerator : MonoBehaviour
     public GameObject enemyPrefab;
     void Start()
     {
-        Instantiate(enemyPrefab, transform.position, transform.rotation);
+        InvokeRepeating("Spawn", 2f, 0.5f);
     }
 
     // Update is called once per frame
@@ -15,4 +15,18 @@ public class EnemyGenerator : MonoBehaviour
     {
         
     }
+    void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-2.8f, 2.8f),
+            transform.position.y, 
+            transform.position.z
+            );
+        Instantiate(
+            enemyPrefab, 
+            spawnPosition, 
+            transform.rotation
+            );
+    }
+
 }
