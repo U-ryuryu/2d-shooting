9eeea49eab0d79ca36e994827c1b75db6224c1fd
U-ryuryu <kugyuryuryu@gmail.com>
2021-05-19 10:15:48 +0900

    敵の撃破

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -15,4 +15,17 @@ public class EnemyShip : MonoBehaviour
     {
         transform.position -= new Vector3(0, Time.deltaTime, 0);
     }
+
+    // isTriggerにチェックをつけた場合こちらが実行される。
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ぶつかったよ");
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+    /* isTriggerにチェックをつけてない場合こちらが実行される。
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+    */
 }
